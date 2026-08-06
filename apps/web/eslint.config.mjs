66522a01d5a565d605927e562387a39938a23295
Looped <nonlooped@gmail.com>
2026-08-06 19:12:32 +0300

    chore(web): ignore design sync artifacts

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -5,5 +5,13 @@ import nextTypeScript from "eslint-config-next/typescript";
 export default defineConfig([
   ...nextVitals,
   ...nextTypeScript,
-  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+  globalIgnores([
+    ".design-sync/**",
+    ".ds-sync/**",
+    ".next/**",
+    "ds-bundle/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
 ]);
